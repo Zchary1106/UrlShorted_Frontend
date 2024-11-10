ba9c1a6cdfdfb0cc41f0b735b36937ab558cdb71
zhaiyadong <zhaiyadong@stu.xjtu.edu.cn>
2024-11-10 16:49:22 +0800

    upload the fronet end code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import CenteredInput from './ShortUrl';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CenteredInput />
+    </div>
+  );
+}
+
+export default App;
